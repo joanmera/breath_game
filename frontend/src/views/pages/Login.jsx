a1f4ac58d0bdb8b70713f2e88f14346fb5dc03c6
Navas-Kevin <kanavas@puce.edu.ec>
2024-08-03 12:03:30 -0500

    tryyy

--- frontend/src/views/pages/Login.jsx
+++ frontend/src/views/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Login.css';
 import { Link, Outlet} from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+
 
 const LoginPage = () => {
   const [correo, setEmail] = useState('');
